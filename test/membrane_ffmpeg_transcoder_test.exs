a0d30232a605481b7ea52670f7485b9249604c0c
Philip Giuliani <philip.giuliani@keepinmind.info>
2024-09-24 10:20:53 +0200

    Remove unused files

--- test/membrane_ffmpeg_transcoder_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule MembraneFfmpegTranscoderTest do
-  use ExUnit.Case
-  doctest MembraneFfmpegTranscoder
-
-  test "greets the world" do
-    assert MembraneFfmpegTranscoder.hello() == :world
-  end
-end
